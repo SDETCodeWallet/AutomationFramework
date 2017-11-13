ffecedc225581807ead34c83466bcd9a4cc99626
TestingGroup360 <shubhamassociateqa@gmail.com>
2017-11-13 23:02:53 +0530

    Excel genric

--- src/main/java/genric/ExcelGenric.java
+++ src/main/java/genric/ExcelGenric.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -12,6 +14,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -21,57 +24,61 @@ public class ExcelGenric {
 
 	FileInputStream fInStream;
 	FileOutputStream fOutStream;
-	XSSFWorkbook xssfbook;
-	HSSFWorkbook hssfBook;
+	Workbook book;
 	Sheet sht;
+	Row rw;
+	Cell cell;
 	String cellValue;
 
-	public String readExcel(int sheetAt, int rowNum, int columnNum) {
-		String[] excelPath = Constants.excelTestFilePath.split("\\.");
-		String extension = excelPath[1];
-		System.out.println(extension);
-		try {
-			fInStream = new FileInputStream(new File((Constants.excelTestFilePath)));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+	public String checkExcelFormat(String excelPath) throws FileNotFoundException {
 
+		String[] exelSplitFile = excelPath.split("\\.");
+		String extension = exelSplitFile[1];
 		if (extension.equalsIgnoreCase("xlsx")) {
-			try {
-				xssfbook = new XSSFWorkbook(fInStream);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			sht = xssfbook.getSheetAt(sheetAt);
-			cellValue = sht.getRow(rowNum).getCell(columnNum).getStringCellValue();
+			return extension;
 		} else if (extension.equalsIgnoreCase("xls")) {
-			try {
-				hssfBook = new HSSFWorkbook(fInStream);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			sht = hssfBook.getSheetAt(sheetAt);
-			cellValue = sht.getRow(rowNum).getCell(columnNum).getStringCellValue();
-
+			return extension;
 		}
-		return cellValue;
+		return extension;
 
 	}
 
-	
-	public void writeExcelValue(String string, String sheetName, int rowNum, int cellNum, String cellvalue) throws EncryptedDocumentException, InvalidFormatException, IOException  {
-		
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		FileOutputStream out = new FileOutputStream(new File("C:\\Users\\shubhamg\\git\\AutomationFramework\\src\\main\\resources\\test.xls"));
-		workbook.write(out);
-		out.close();
-		
+	public String readExcelValue(String excelPath, int sheetAt, int rowNum, int columnNum) throws IOException {
+		fInStream = new FileInputStream(new File(excelPath));
+		if (new ExcelGenric().checkExcelFormat(excelPath).equalsIgnoreCase("xlsx")) {
+			book = new XSSFWorkbook(fInStream);
+		} else if (new ExcelGenric().checkExcelFormat(excelPath).equalsIgnoreCase("xls")) {
+			book = new HSSFWorkbook(fInStream);
+		}
+		return book.getSheetAt(sheetAt).getRow(rowNum).getCell(columnNum).getStringCellValue();
+	}
+
+	public List<String> getColumnsValue(String excelPath, int sheetAt, int rowNum, int columnNum) throws IOException {
+		fInStream = new FileInputStream(new File(excelPath));
+		List<String> listValues = null;
+		if (new ExcelGenric().checkExcelFormat(excelPath).equalsIgnoreCase("xlsx")) {
+			book = new XSSFWorkbook(fInStream);
+		} else if (new ExcelGenric().checkExcelFormat(excelPath).equalsIgnoreCase("xls")) {
+			book = new HSSFWorkbook(fInStream);
+		}
+		int values = book.getSheetAt(sheetAt).getLastRowNum();
+		for (int i = 0; i < values - 1; i++) {
+			String value = book.getSheetAt(sheetAt).getRow(i).getCell(columnNum).getStringCellValue();
+			System.out.println(value);
+			listValues = new ArrayList<String>();
+			listValues.add(value);
+			
+
+		}
+		return listValues;
+
 	}
 
 	public static void main(String[] args) throws EncryptedDocumentException, InvalidFormatException, IOException {
-
-		new ExcelGenric().writeExcelValue("src\\main\\resources\\shubhmTestingPurpose.xlsx", "Yesthjkj", 0, 0,
-				"my name is khan");
+		List<String> value = new ExcelGenric().getColumnsValue(Constants.excelTestFilePath, 0, 0, 0);
+		for (String test : value) {
+			System.out.println(test);
+		}
 
 	}
 
