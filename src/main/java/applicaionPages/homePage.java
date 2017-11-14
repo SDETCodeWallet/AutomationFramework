ecac8e8dcb653586d3e38f23fb9498d70cd50f0f
TestingGroup360 <shubhamassociateqa@gmail.com>
2017-11-14 22:59:04 +0530

    Genric files

--- src/main/java/applicaionPages/homePage.java
+++ src/main/java/applicaionPages/homePage.java
@@ -1,14 +1,11 @@
 package applicaionPages;
 
-import java.util.List;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import genric.WebDriverUtils;
-import genric.baseTest;
 
 public class homePage extends WebDriverUtils {
 
